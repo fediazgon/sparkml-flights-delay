6a00f34b5bc14e53119f65273bc91c4f17caaa3a
Giorgio Ruffa <gioruffa@gmail.com>
2017-11-22 16:10:35 +0100

    added scopt for line argumens

--- src/main/scala/upm/bd/FlightDelayApp.scala
+++ src/main/scala/upm/bd/FlightDelayApp.scala
@@ -1,7 +1,19 @@
+import org.rogach.scallop._
+
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+//  val apples = opt[Int](required = true)
+//  val bananas = opt[Int]()
+  val rawFilePath = trailArg[String](required = true)
+  verify()
+}
+
 object FlightDelayApp {
   def main(args : Array[String]): Unit =
   {
     println("hello World!")
+    val conf = new Conf(args)
+    println(s"Using file ${conf.rawFilePath.getOrElse("")}")
+    
   }
 
 }
