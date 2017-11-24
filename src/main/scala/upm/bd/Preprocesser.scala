6d9d9700046366f2cc12e2a0a2d6472e3ee474c1
Giorgio Ruffa <gioruffa@gmail.com>
2017-11-24 14:52:09 +0100

    preprocessing stage 1

--- src/main/scala/upm/bd/Preprocesser.scala
+++ src/main/scala/upm/bd/Preprocesser.scala
@@ -3,7 +3,9 @@ package upm.bd
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 
-class Preprocesser {
+class Preprocesser(verbose: Boolean = true) {
+
+
   def preprocess(filePath : String) : DataFrame =
   {
     import SparkSessionWrapper.spark.implicits._
@@ -16,7 +18,20 @@ class Preprocesser {
       .withColumn("Cancelled", $"Cancelled".cast("int")) //keeping it as an int to exploit aggregation functions
       .withColumn("Diverted",$"Diverted".cast("int"))
       .withColumn("TaxiOut",$"TaxiOut".cast("int")) //we can use it because we havent still took off
+
+    val forbiddenVariables = Seq("ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn", "Diverted", "CarrierDelay",
+      "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay")
+    df = df.drop(forbiddenVariables:_*) //like args builtin in python
+
+    showDf(df)
+
     df
   }
+
+  private def showDf(dataFrame: DataFrame, lines :Int = 10) : Unit =
+  {
+    if(verbose) dataFrame.show(lines)
+  }
+
 
 }
