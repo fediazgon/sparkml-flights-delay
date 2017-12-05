027e7649b292e7d44c2e5fa5d42c43ade478bec4
Fernando Diaz <fediazgon@gmail.com>
2017-12-05 01:14:26 +0100

    Change to TrainValidation

--- src/main/scala/upm/bd/pipelines/RandomForestPipeline.scala
+++ src/main/scala/upm/bd/pipelines/RandomForestPipeline.scala
@@ -1,12 +1,11 @@
 package upm.bd.pipelines
 
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{StringIndexer, VectorIndexer}
 import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-import org.apache.spark.sql.{Dataset, Row}
-import upm.bd.transformers.{FeaturesCreator, Indexer, Preprocesser}
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions._
+import upm.bd.transformers.{FeaturesCreator, Indexer, Preprocesser}
 import upm.bd.utils.MyLogger
 
 
@@ -16,23 +15,22 @@ class RandomForestPipeline(val data: Dataset[_]) {
 
 
   val FEATURES_COL_NAMES = Array(
-//    "Month",
-//    "DayofMonth",
-//    "DayOfWeek",
-//    "WeekEnd",
+    //    "Month",
+    //    "DayofMonth",
+    //    "DayOfWeek",
+    //    "WeekEnd",
     "Distance",
     "TaxiOut",
-//    "UniqueCarrierIndex",
-//    "OriginIndex",
-//    "DestIndex",
-//    "CRSDepTimeMin",
+    //    "UniqueCarrierIndex",
+    //    "OriginIndex",
+    //    "DestIndex",
+    //    "CRSDepTimeMin",
     "DepDelay",
     "DepTimeMin"
-//    "CRSElapsedTime"
+    //    "CRSElapsedTime"
   )
 
-  def run() : Unit =
-  {
+  def run(): Unit = {
     import upm.bd.utils.SparkSessionWrapper.spark.implicits._
 
     val preprocesser = new Preprocesser(verbose = false)
@@ -43,18 +41,18 @@ class RandomForestPipeline(val data: Dataset[_]) {
     val featuresCreator = new FeaturesCreator(FEATURES_COL_NAMES)
     val featuresDf = featuresCreator.transform(indexedDf)
 
-//    val indexerModel = new VectorIndexer()
-//      .setInputCol("features")
-//      .setOutputCol("indexed")
-//      .setMaxCategories(10)
-//      .fit(featuresDf)
+    //    val indexerModel = new VectorIndexer()
+    //      .setInputCol("features")
+    //      .setOutputCol("indexed")
+    //      .setMaxCategories(10)
+    //      .fit(featuresDf)
 
-//    val finalData = indexerModel.transform(featuresDf)//.select($"indexed",$"ArrDelay")
+    //    val finalData = indexerModel.transform(featuresDf)//.select($"indexed",$"ArrDelay")
     var finalData = featuresDf
     MyLogger.info("Final Data")
-    finalData.show(100,truncate = false)
+    finalData.show(100, truncate = false)
 
-    finalData = finalData.select($"features",$"ArrDelay")
+    finalData = finalData.select($"features", $"ArrDelay")
 
 
     val Array(training, test) = finalData.randomSplit(Array(0.7, 0.3))
@@ -62,7 +60,7 @@ class RandomForestPipeline(val data: Dataset[_]) {
     val model = new RandomForestRegressor()
       .setLabelCol("ArrDelay")
       .setFeaturesCol("features")
-//      .setMaxBins(1000)
+    //      .setMaxBins(1000)
 
     val evaluator = new RegressionEvaluator()
       .setLabelCol("ArrDelay")
@@ -84,11 +82,12 @@ class RandomForestPipeline(val data: Dataset[_]) {
 
     MyLogger.printHeader("TESTING")
     cvModel.transform(test)
-      .select($"ArrDelay",$"prediction", (abs($"ArrDelay" - $"prediction")).as("residual")).select(mean($"residual").as("MAE"), stddev($"residual")).show()
+      .select($"ArrDelay", $"prediction", (abs($"ArrDelay" - $"prediction")).as("residual")).select(mean($"residual").as("MAE"), stddev($"residual")).show()
     //    .select("id", "text", "probability", "prediction")1
     //    .collect()
     //    .foreach { case Row(id: Long, text: String, prob: Vector, prediction: Double) =>
     //      println(s"($id, $text) --> prob=$prob, prediction=$prediction")
     //    }
   }
+
 }
